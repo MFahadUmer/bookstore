6363e1ab829a9d6a8a6f4f7e66f3603b33b5c8f9
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-01-06 19:04:15 +0500

    Modified categories Map

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const CATEGORIES = ['Select Category', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 const BooksForm = () => (
   <div>
     <form>
       <input type="text" placeholder="Book title" />
       <select name="categories" id="categories">
+        <option value="">Select Category</option>
         {CATEGORIES.map(value => (
           <option key={value} value={value}>{value}</option>
         ))}
