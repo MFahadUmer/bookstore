ba869fdd3423da9c1d65c0597985c30163ba9ade
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-01-06 21:20:26 +0500

    Migrate Redux setup from App to index

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import BooksList from './BooksList';
 import BooksForm from './BooksForm';
-import store from '../reducers';
 
 const App = () => (
-  <Provider store={store}>
-    <div className="app">
-      <BooksList />
-      <BooksForm />
-    </div>
-  </Provider>
+  <div className="app">
+    <BooksList />
+    <BooksForm />
+  </div>
 );
 
 export default App;
