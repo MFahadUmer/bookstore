ca5679ac52b4753042efa038209d181a964c3d74
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-01-08 19:07:03 +0500

    Adds states title and category

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-const BooksForm = () => (
-  <div>
-    <form>
-      <input type="text" placeholder="Book title" />
-      <select name="categories" id="categories">
-        <option value="">Select Category</option>
-        {CATEGORIES.map(value => (
-          <option key={value} value={value}>{value}</option>
-        ))}
-      </select>
-      <input type="submit" value="Add Book" />
-    </form>
-  </div>
-);
+const BooksForm = () => {
+  const [title, seTitle] = useState('');
+  const [category, setCategory] = useState('');
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Book title" />
+        <select name="categories" id="categories">
+          <option value="">Select Category</option>
+          {CATEGORIES.map(value => (
+            <option key={value} value={value}>{value}</option>
+          ))}
+        </select>
+        <input type="submit" value="Add Book" />
+      </form>
+    </div>
+  );
+};
 
 export default BooksForm;
