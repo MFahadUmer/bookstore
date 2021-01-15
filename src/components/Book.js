64941e3b75c39aaa1df0f65f65c7a968bdf43d14
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-01-15 09:25:35 +0500

    Adds Styles for Book Details

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,14 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = ({ book, removeBook }) => (
-  <tr>
-    <td>{book.bookId}</td>
-    <td>{book.bookTitle}</td>
-    <td>{book.bookCategory}</td>
-    <td>
-      <button type="submit" onClick={() => removeBook(book.bookId)}>Remove Book</button>
-    </td>
-  </tr>
+  <div className="booksMainContainer">
+    <div className="booksContainer">
+      <div className="bookDetails">
+        <p className="bookCategory">
+          {book.bookCategory}
+        </p>
+        <p className="bookTitle">
+          {book.bookTitle}
+        </p>
+        <p className="bookAuthor">
+          Author Name
+        </p>
+        <div className="bookContainerFooter">
+          <button className="actionsOnBook" type="submit">Comments</button>
+          <span className="separator">|</span>
+          <button className="actionsOnBook" type="submit" onClick={() => removeBook(book.bookId)}>Remove</button>
+          <span className="separator">|</span>
+          <button className="actionsOnBook" type="submit">Edit</button>
+        </div>
+      </div>
+      <div className="bookReadGraph">
+        graph
+      </div>
+      <div className="bookReadUpdate">
+        update Books Read Status
+      </div>
+    </div>
+  </div>
 );
 
 Book.propTypes = {
